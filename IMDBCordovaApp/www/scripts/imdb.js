ae6082ff7dbc22037e324f2bee67f69fcb0f4363
Jitu <Jitu@JITU-PC>
2016-08-23 23:57:22 +0530

    Add project files.

---
+++ IMDBCordovaApp/www/scripts/imdb.js
@@ -0,0 +1,34 @@
+﻿
+
+function getMovieDetails() {
+
+    var movieName = $('#movie-name-input').val();
+
+    var queryString =
+        'http://www.omdbapi.com/?s='
+        + movieName;
+
+    $.ajax({
+        url: queryString,
+        type: 'GET',
+        dataType: 'json',
+        success: function (result) {
+            $('#error-msg').hide();
+            $('#movie-data').show();
+
+            $('#name').html(result.Search[0].Title);
+            $('#year').html(result.Search[0].Year);
+
+
+        },
+        error: function () {
+            $('#movie-data').hide();
+            $('#error-msg').show();
+            $('#error-msg').html('Error fetching data');
+        }
+    });
+
+    return false;
+}
+
+
---
+++ IMDBCordovaApp/www/scripts/index.js
@@ -0,0 +1,34 @@
+﻿/// <reference path="../../Scripts/jquery-3.1.0.min.js" />
+// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkID=397704
+// To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
+// and then run "window.location.reload()" in the JavaScript Console.
+(function () {
+    "use strict";
+
+
+    document.addEventListener('deviceready', onDeviceReady.bind(this), false);
+
+    function onDeviceReady() {
+        // Handle the Cordova pause and resume events
+        document.addEventListener('pause', onPause.bind(this), false);
+        document.addEventListener('resume', onResume.bind(this), false);
+
+        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
+        $('#mylink').css('display', 'none');
+
+
+        $('#get-movie-btn').click(getMovieDetails);
+
+    };
+
+    //8aee82e91e41b6c79237e81cb23c4240
+
+    function onPause() {
+        // TODO: This application has been suspended. Save application state     here.
+    };
+
+    function onResume() {
+        // TODO: This application has been reactivated. Restore application     state here.
+    };
+})();
